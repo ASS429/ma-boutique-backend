ca834bfa33ca114affb1e2672002a5b87001cc23
ASS429 <sanarfang429@gmail.com>
2025-08-12 14:02:36 +0000

    db.js

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST, // IPv4 uniquement
+  port: 5432,
+  database: process.env.DB_NAME,
   ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
