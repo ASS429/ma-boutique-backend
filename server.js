1084afb957fd9d1b8030a8bde2585430109c6593
ASS429 <sanarfang429@gmail.com>
2025-08-13 00:24:13 +0000

    server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use('/categories', require('./routes/categories'));
 app.use('/sales', require('./routes/sales'));
 app.use('/tontines', require('./routes/tontines'));
 app.use('/stats', require('./routes/stats'));
+app.use('/auth', require('./routes/auth'));
+
 
 // Test route
 app.get('/', (req, res) => {
